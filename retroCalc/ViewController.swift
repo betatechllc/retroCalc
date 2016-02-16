221de5ad7d65a0b4c30f18f28b0fe11e94688c91
betatechllc <tadams@betatech.co>
2016-02-16 09:11:54 -0600

    UI done, starting to code logic

--- retroCalc/ViewController.swift
+++ retroCalc/ViewController.swift
@@ -7,19 +7,76 @@
 //
 
 import UIKit
+import AVFoundation
 
 class ViewController: UIViewController {
-
+    
+    enum Operation: String {
+        case Divide = "/"
+        case Multiply = "*"
+        case Subtract = "-"
+        case Add = "+"
+        case Equals = "="
+        case Empty = "Empty"
+    }
+    
+    
+    var btnSound: AVAudioPlayer!
+    
+    var runningNumber = ""
+    var leftValStr = ""
+    var rightValStr = ""
+    var currentOperation: Operation = Operation.Empty
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        loadSoundPlayer()
+        
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    
+    // IBOutlets
+    @IBOutlet var outputLabel: UILabel!
+    
+    // IBActions
+    @IBAction func numberPressed(btn: UIButton!) {
+        btnSound.play()
+        
+        runningNumber += "\(btn.tag)"
+        outputLabel.text = runningNumber
     }
-
+    
+    @IBAction func onDividePressed(sender: UIButton!) {
+        
+    }
+    
+    @IBAction func onMultiplyPressed(sender: UIButton!) {
+        
+    }
+    
+    @IBAction func onSubtractPressed(sender: UIButton!) {
+        
+    }
+    
+    @IBAction func onAddPressed(sender: UIButton!) {
+        
+    }
+    
+    @IBAction func onEqualPressed(sender: UIButton!) {
+        
+    }
+    
+    // Functions (Methods)
+    func loadSoundPlayer() {
+        let path = NSBundle.mainBundle().pathForResource("btn", ofType: "wav")
+        let soundUrl = NSURL(fileURLWithPath: path!)
+        
+        do {
+            try btnSound = AVAudioPlayer(contentsOfURL: soundUrl)
+            btnSound.prepareToPlay()
+        } catch let err as NSError {
+            print(err.debugDescription)
+        }
+    }
 
 }
 
